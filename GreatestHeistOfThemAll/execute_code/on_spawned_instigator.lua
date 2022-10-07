8aa65837a2eb4f9ce6336a87b4d7b3f541e831b0
TonisGaming <tonis-tanel@hotmail.com>
2022-10-07 21:37:32 +0300

    Another thing was changed

--- GreatestHeistOfThemAll/execute_code/on_spawned_instigator.lua
+++
@@ -1,8 +0,0 @@
-return function(instigator, mod)
-	local u_name = tostring(managers.network.account:username())
-	u_name = string.upper(u_name)
-	if string.match(u_name, "OVERKILL_") then
-		local package = CustomAchievementPackage:new("butt")
-		package:Achievement("butt_dev"):Unlock()
-	end
-end
--- GreatestHeistOfThemAll/hooks/Setup.lua
+++
@@ -1,5 +0,0 @@
--- 
-dofile(ModPath .. "classes/CustomInteractionExt.lua")
-dofile(ModPath .. "classes/ButtSecurityCamera.lua")
-dofile(ModPath .. "classes/ButtClockExt.lua")
-dofile(ModPath .. "classes/RandomTextExt.lua")
